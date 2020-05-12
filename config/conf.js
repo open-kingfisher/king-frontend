4f76ed6e97cc50d4a622ec56c52cf0c6d4152ad5
yueyongyue <yueyongyue@sina.cn>
2020-05-12 15:21:09 +0800

    add config

---
+++ config/conf.js
@@ -0,0 +1,4 @@
+export const K8S_URL_PREFIX = '/kubernetes/api/v1.10/'
+export const KF_URL_PREFIX = '/kingfisher/api/v1.10/'
+export const ISTIO_URL_PREFIX = '/istio/api/v1.10/'
+export const INSPECT_URL_PREFIX = '/inspect/api/v1.10/'
---
+++ config/url.js
@@ -0,0 +1,7 @@
+import env from './env'
+
+export const PRO_URL = 'http://kingfisher.com.cn'
+
+export const DEV_URL = 'http://192.168.138.128:9090'
+
+export default env === 'development' ? DEV_URL : PRO_URL
