75997309f0a941b30611494b8e8cb0da9ea5e1b4
kr521 <macao450@163.com>
2020-05-26 18:42:46 +0800

    修改dockerfile打包方式

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,5 @@ echo "Asia/Shanghai" > /etc/timezone
 ln -sf /usr/share/zoneinfo/ /etc/localtime
 [ "$DOMAIN" ] || DOMAIN="kingfisher.com"
 sed -i "s/kingfisher.com.cn/$DOMAIN/g" /var/www/html/
+sed -i "s/kingfisher.com.cn/$DOMAIN/g" /var/www/html/css/
 nginx -g "daemon off;"
