94a16437eb79756797fd684bf0b103a6d2c5a7b0
kr521 <macao450@163.com>
2020-05-27 11:16:55 +0800

    修改Dockerfile编译方式

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
-echo "Asia/Shanghai" > /etc/timezone 
-ln -sf /usr/share/zoneinfo/ /etc/localtime
 [ "$DOMAIN" ] || DOMAIN="kingfisher.com"
+[ "$TIME_ZONE" ] || TIME_ZONE="Asia/Shanghai"
+[ "$ALPINE_REPO" ] || ALPINE_REPO="mirrors.aliyun.com"
+
+sed -i 's/dl-cdn.alpinelinux.org/${ALPINE_REPO}/g' /etc/apk/repositories     
+apk --no-cache add tzdata 
+echo "${TIME_ZONE}" > /etc/timezone 
+ln -sf /usr/share/zoneinfo/${TIME_ZONE} /etc/localtime 
+mkdir /lib64 
+ln -s /lib/libc.musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2
+
+
 sed -i "s/kingfisher.com.cn/$DOMAIN/g" `grep "kingfisher.com.cn" -rl /var/www/html/`
 nginx -g "daemon off;"
